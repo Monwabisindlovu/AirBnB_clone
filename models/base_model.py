9b2f3fd9a008882dd3f1d1b110232e2ece4dd38f
LusterPearl <Salamioluwatosin90@gmail.com>
2023-11-08 02:08:29 -0800

    updated

--- models/base_model.py
+++ models/base_model.py
@@ -22,8 +22,6 @@ class BaseModel:
                                                   "%Y-%m-%dT%H:%M:%S.%f"))
                     else:
                         setattr(self, key, value)
-                else:
-                    setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
