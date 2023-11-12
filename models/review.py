4a6f0dae84de04d1d24073539c2e2cae7f8d4e53
LusterPearl <Salamioluwatosin90@gmail.com>
2023-11-12 05:39:42 -0800

    updates

--- models/review.py
+++ models/review.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-""" The reviews """
+""" The Review class is a subclass of the BaseModel class
+and represents a review associated with a certain entity,
+such as a place or a service. """
 
 from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-    """Review class """
+    """Review class of BaseModel """
     place_id = ""
     user_id = ""
     text = ""
